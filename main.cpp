22e03ecb90b41f5e0c0537e5a70d43dbfea7404c
tumax7 <73022920+tumax7@users.noreply.github.com>
2022-11-26 19:49:45 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//struct coordinates int 
+
 void print_sudoku(int sudoku[9][9]){
 	
 	for (int i=0; i<9; i++){
@@ -48,20 +50,65 @@ bool solved(int sudoku[9][9]){
 	return true;
 }
 
+bool possible_value_position(int sudoku[9][9], int row,int col,int val){
 
-// To Do
-void detect_0_pos(int sudoku[9][9]){
-	
-	for (int i=0; i<9; i++){
-		for (int j=0;j<9;j++){
-			cout<<sudoku[i][j]<<' ';
-			
-			}
-			cout <<'\n';
+	// Check row
+	for (int i=0;i<9;i++){
+		if (sudoku[row][i]==val){
+			return false;
+		}
+	}
+	// Check col
+	for (int i=0;i<9;i++){
+		if (sudoku[i][col]==val){
+			return false;
 		}
 	}
 
+	//Check square
+	for (int i=(row - row%3);i<(row-row%3+3);i++){
+		for (int j=(col - col%3);j<(col-col%3+3);j++){
+			if (sudoku[i][j]==val){
+				return false;
+			}
+		}
+	}
+	return true;
+}
 
+bool solve(int sudoku[9][9],int row, int col){
+
+	//End case
+	if (row==8&&col==9){
+		return true;		
+	}
+
+	//Roll over to next row
+	if (col==9){
+		col=0;
+		row +=1; 
+	}
+
+	// If not empty continue
+
+	if (sudoku[row][col]>0){
+		return solve(sudoku,row,col+1);
+	}
+
+	// Start solving
+	for (int i=1; i<=9; i++){
+		if (possible_value_position(sudoku,row,col,i)){
+			sudoku[row][col] = i;
+
+			if (solve(sudoku,row,col+1)){
+				return true;
+			}
+		}
+		sudoku[row][col] = 0;
+	}
+	
+	return false;
+}
 
 
 int main(){
@@ -74,6 +121,9 @@ int main(){
 	                    {1,9,4,8,3,6,5,7,2},
 			    {5,6,7,2,1,4,3,8,9},
 			    {2,0,8,5,7,9,4,6,1}};
+	print_sudoku(sudoku);
+	cout << solved(sudoku)<< '\n';
+	solve(sudoku,0,0);
 	print_sudoku(sudoku);
 	cout << solved(sudoku)<< '\n';
 	return 0;
